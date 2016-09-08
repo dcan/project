ea80cc45993ddaa2c4de008e597381fd8c55e3ca
Diyar Can <diyar.can1@gmail.com>
2016-09-08 17:24:07 +0200

    Ajout d'une section sur la prospective, creation de la vitrine

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 	<head>
 		<title>proGective</title>
 		<link rel="stylesheet" href="assets/css/bootstrap.css" />
+		<link rel="stylesheet" href="assets/css/add.css" />
 		<script type="text/javascript" src="assets/js/bootsrap.js"></script>
 		<script type="text/javascript" src="jquery-1.12.4.min.js"></script>
 	</head>
 
 	<body>
+
+		<!-- Navbar -->
 		<nav class="navbar navbar-default">
 		  <div class="container">
 
@@ -21,13 +24,22 @@
 			   	<ul class="nav navbar-nav">
 			   		<li class="active"><a href="#">Blog</a></li>
 			   		<li><a href="#">Ressources</a></li>
-			   		<li><a href="#">À propos</a></li>
+			   		<li><a href="#quisommesnous">À propos</a></li>
 			   		<li><a href="#">Contact</a></li>
 			   	</ul>
 			</div>
 
 		  </div>
 		</nav>
+		<!-- Fin navbar -->
+
+		<div class="container">
+			<h1 class="h-balise" id="quest-ce-que-la-prospective">Qu'est ce que la prospective ?</h1>
+			<div class="text">
+				<p>Text</p>
+			</div>
+			<hr>
+		</div>
 	</body>
 
 </html>
