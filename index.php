3b17e25d55ed39a96005875f3f9e74959e10a0a5
Diyar Can <diyar.can1@gmail.com>
2016-09-08 16:40:17 +0200

    Ajout du style et creation d'une navbar

---
+++ index.php
@@ -0,0 +1,33 @@
+<!Doctype html>
+<html>
+	<head>
+		<title>proGective</title>
+		<link rel="stylesheet" href="assets/css/bootstrap.css" />
+		<script type="text/javascript" src="assets/js/bootsrap.js"></script>
+		<script type="text/javascript" src="jquery-1.12.4.min.js"></script>
+	</head>
+
+	<body>
+		<nav class="navbar navbar-default">
+		  <div class="container">
+
+		   	<div class="navbar-header">
+		   		<a class="navbar-brand" href="#">
+		   			proGective
+		   		</a>
+		   	</div>
+
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			   	<ul class="nav navbar-nav">
+			   		<li class="active"><a href="#">Blog</a></li>
+			   		<li><a href="#">Ressources</a></li>
+			   		<li><a href="#">À propos</a></li>
+			   		<li><a href="#">Contact</a></li>
+			   	</ul>
+			</div>
+
+		  </div>
+		</nav>
+	</body>
+
+</html>
