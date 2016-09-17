759a00ca85f8e728652c75f7ec435e19df721cce
Diyar CAN <diyar.can1@gmail.com>
2016-09-17 17:01:51 +0200

    Creation du corps de la page blog

--- blog.php
+++ blog.php
@@ -34,6 +34,41 @@
 		  </div>
 		</nav>
 		<!-- Fin navbar -->
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-md-9"></div>
+				<div class="col-xs-6 col-md-3">
+					<form role="form" method="POST" action="search.php" > 
+					  <div class="form-group has-feedback has-feedback-left">
+					    <input type="text" class="form-control" placeholder="Search" />
+					    <i class="form-control-feedback glyphicon glyphicon-search"></i>
+					  </div>
+					</form>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-9">
+					<div class="well well-lg">
+						<div class="page-header">
+						  <h1>New robot <small>by Andrew</small></h1>
+						</div>
+						<p>Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit.
+
+						Ut enim quisque sibi plurimum confidit et ut quisque maxime virtute et sapientia sic munitus est, ut nullo egeat suaque omnia in se ipso posita iudicet, ita in amicitiis expetendis colendisque maxime excellit. Quid enim? Africanus indigens mei? Minime hercule! ac ne ego quidem illius; sed ego admiratione quadam virtutis eius, ille vicissim opinione fortasse non nulla, quam de meis moribus habebat, me dilexit; auxit benevolentiam consuetudo. Sed quamquam utilitates multae et magnae consecutae sunt, non sunt tamen ab earum spe causae diligendi profectae.
+
+						Pandente itaque viam fatorum sorte tristissima, qua praestitutum erat eum vita et imperio spoliari, itineribus interiectis permutatione iumentorum emensis venit Petobionem oppidum Noricorum, ubi reseratae sunt insidiarum latebrae omnes, et Barbatio repente apparuit comes, qui sub eo domesticis praefuit, cum Apodemio agente in rebus milites ducens, quos beneficiis suis oppigneratos elegerat imperator certus nec praemiis nec miseratione ulla posse deflecti.</p>
+					</div>
+				</div>
+			</div>
+
+			<nav aria-label="...">
+			  <ul class="pager">
+			    <li><a href="#">Previous</a></li>
+			    <li><a href="#">Next</a></li>
+			  </ul>
+			</nav>
+		</div>
 
 	</body>
 
